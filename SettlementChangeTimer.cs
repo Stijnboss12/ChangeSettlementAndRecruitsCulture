f30e815a7a86e13ca150d3ae2e376749fc9eec8d
Stijn van Oudheusden <58070673+Stijnboss12@users.noreply.github.com>
2023-08-31 23:50:18 +0200

    new version

--- SettlementChangeTimer.cs
+++ SettlementChangeTimer.cs
@@ -6,11 +6,13 @@ namespace ChangeSettlementCulture
     public class SettlementChangeTimer
     {
         public uint SettlementId { get; set; }
+        public string SettlementName { get; set; }
         public int DaysSinceOwnerChanged { get; set; } = 0;
 
-        public SettlementChangeTimer(uint settlementId)
+        public SettlementChangeTimer(uint settlementId, string settlementName)
         {
             SettlementId = settlementId;
+            SettlementName = settlementName;
         }
     }
 }
